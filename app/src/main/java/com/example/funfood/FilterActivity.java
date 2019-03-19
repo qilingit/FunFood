c09235c0f88c1d085373dc8b22f8b03862290017
Qilin ZHANG <nicolas.qilin.zhang@gmail.com>
2019-03-19 18:41:32 +0100

    Spinner array finished

---
+++ app/src/main/java/com/example/funfood/FilterActivity.java
@@ -0,0 +1,36 @@
+package com.example.funfood;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class FilterActivity extends Activity {
+
+    Spinner spinnerType;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filter);
+        initSpinner();
+
+    }
+
+    private void initSpinner() {
+        //Récupération des Spinners déclarés dans le fichier activity_filter.xml de res/layout
+        spinnerType = findViewById(R.id.spinnerType);
+
+
+        /*On passe à l'adapate le context, le tableau de string et un fichier de presentation par défaut*/
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner_array, android.R.layout.simple_spinner_item);
+
+        /* On definit une présentation du spinner quand il est déroulé (android.R.layout.simple_spinner_dropdown_item) */
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        //Enfin on passe l'adapter au Spinner
+        spinnerType.setAdapter(adapter);
+    }
+}
--- app/src/main/java/com/example/funfood/MainActivity.java
+++ app/src/main/java/com/example/funfood/MainActivity.java
@@ -6,7 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -16,6 +18,7 @@ import com.google.android.gms.common.GooglePlayServicesRepairableException;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private static final int ERROR_DIALOG_REQUEST = 9001;
+    private Spinner spinnerType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +28,25 @@ public class MainActivity extends AppCompatActivity {
         if(isServicesOK()) {
             init();
         }
+        //initSpinner();
     }
 
 
+    private void initSpinner() {
+        //Récupération des Spinners déclarés dans le fichier activity_filter.xml de res/layout
+        spinnerType = findViewById(R.id.spinnerType);
+
+
+        /*On passe à l'adapate le context, le tableau de string et un fichier de presentation par défaut*/
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner_array, android.R.layout.simple_spinner_item);
+
+        /* On definit une présentation du spinner quand il est déroulé (android.R.layout.simple_spinner_dropdown_item) */
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        //Enfin on passe l'adapter au Spinner
+        spinnerType.setAdapter(adapter);
+    }
 
     private void init() {
         /*Button btnMap = findViewById(R.id.part1);
