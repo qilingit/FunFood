5bb9faa9d5e41e31751f38394e4d8ee5f9c7e962
Qilin ZHANG <nicolas.qilin.zhang@gmail.com>
2019-03-26 15:04:14 +0100

    correct problem of git version

--- app/src/main/java/com/example/funfood/FilterActivity.java
+++ app/src/main/java/com/example/funfood/FilterActivity.java
@@ -14,24 +14,21 @@ public class FilterActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
-<<<<<<< HEAD
 
-=======
         initSpinner();
 
     }
 
     private void initSpinner() {
->>>>>>> qilin
+
         //Récupération des Spinners déclarés dans le fichier activity_filter.xml de res/layout
         spinnerType = findViewById(R.id.spinnerType);
 
 
-<<<<<<< HEAD
 		/*On passe à l'adapate le context, le tableau de string et un fichier de presentation par défaut*/
-=======
+
         /*On passe à l'adapate le context, le tableau de string et un fichier de presentation par défaut*/
->>>>>>> qilin
+
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.spinner_array, android.R.layout.simple_spinner_item);
 
@@ -40,9 +37,6 @@ public class FilterActivity extends Activity {
 
         //Enfin on passe l'adapter au Spinner
         spinnerType.setAdapter(adapter);
-<<<<<<< HEAD
 
-=======
->>>>>>> qilin
     }
 }
--- app/src/main/java/com/example/funfood/MainActivity.java
+++ app/src/main/java/com/example/funfood/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         if(isServicesOK()) {
             init();
         }
-        //initSpinner();
+        initSpinner();
     }
 
 
